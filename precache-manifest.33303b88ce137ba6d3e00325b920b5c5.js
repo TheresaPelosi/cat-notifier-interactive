c38af76114f3258dd7f5a9229a3e5efdb1e985a1
TheresaPelosi <theresa.pelosi@gmail.com>
2020-05-25 12:17:41 -0400

    Updates

---
+++ precache-manifest.33303b88ce137ba6d3e00325b920b5c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef4d61df2b78e51df52e45fd62953c95",
+    "url": "/cat-notifier/index.html"
+  },
+  {
+    "revision": "3bb23629b4c0d46a5260",
+    "url": "/cat-notifier/static/css/main.a9de94e6.chunk.css"
+  },
+  {
+    "revision": "b7f1a184ac989694ef70",
+    "url": "/cat-notifier/static/js/2.b28cc454.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/cat-notifier/static/js/2.b28cc454.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bb23629b4c0d46a5260",
+    "url": "/cat-notifier/static/js/main.e6b808cb.chunk.js"
+  },
+  {
+    "revision": "9b669afaab4f0f1e3fda",
+    "url": "/cat-notifier/static/js/runtime-main.849fedd1.js"
+  }
+]);
